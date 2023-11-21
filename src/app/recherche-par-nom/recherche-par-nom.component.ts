c7fefdf46c49d3ed1b55729d62c920f18d99359e
SyrineBoussetta24 <syrinebouss24@gmail.com>
2023-11-21 20:30:15 +0100

    RechNom

--- src/app/recherche-par-nom/recherche-par-nom.component.ts
+++ src/app/recherche-par-nom/recherche-par-nom.component.ts
@@ -6,17 +6,25 @@ import { Component } from '@angular/core';
   selector: 'app-recherche-par-nom',
   templateUrl: './recherche-par-nom.component.html',
 })
-export class RechercheParNomComponent  {
-  nomInstrument! : string;
-  instruments! : Instrument[];
-  searchTerm! : string ;
-  constructor(){  }
-  ngOnInit() : void {
+export class RechercheParNomComponent {
+  nomInstrument!: string;
+  instruments: Instrument[] = [];
+  searchTerm!: string;
+  allInstruments: Instrument[] = [];
+
+  constructor(private instrumentService: InstrumentService) {}
+
+  ngOnInit(): void {
+    this.allInstruments=this.instrumentService.listeInstruments() ;
+     }
+
+  rechercherInstrs() {
+    this.allInstruments=this.instrumentService.rechercherParNom(this.nomInstrument);
   }
-  rechercherInstrs(){
-   /*  this.InstrumentService.rechercherParNom(this.nomInstrument).
-subscribe(instrs => {
-this.instruments = instrs;
-console.log(instrs)}); */
+
+  onKeyUp(filterText: string) {
+    this.instruments = this.allInstruments.filter(item =>
+      item.nomInstrument!.toLowerCase().includes(filterText)
+    );
   }
 }
